ed57d96f724f548e96314f75110d660157cf6f43
devika2255 <88394698+devika2255@users.noreply.github.com>
2021-12-08 22:34:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            zebodo
+       </title>
+    </head>
+    <body>
+        <?php include ('./components/headerindex.php'); ?>
+        <?php include ('./components/carousel.php'); ?>
+      <?php include ('./components/indexcategory.php');?>
+      <?php include ('./components/footer.php');?>
+
+
+    </body>
+</html>
